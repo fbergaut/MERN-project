9570c258da6b1beb6ca43ca39233a53e52f18d80
fbergaut <f.bergaut@gmail.com>
2021-09-27 16:14:57 +0200

    creation d'une route POST pour user

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/user.routes');
 require('dotenv').config({ path: './config/.env' })
 require('./config/db');
 const app = express();
 
+app.use(bodyParser.json());
+
+//routes
+app.use('/api/user', userRoutes);
+
+// server
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`);
 });
