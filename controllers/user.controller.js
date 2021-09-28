f0c59581be9e4b5efd5401bf84a6a68bd7f37da9
fbergaut <f.bergaut@gmail.com>
2021-09-28 15:30:01 +0200

    create DELETE route for user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,6 +19,7 @@ module.exports.getOneUser = (req, res) => {
 module.exports.updateUser = async(req, res) => {
     if (!ObjectID.isValid(req.params.id))
         return res.status(400).send("ID unknown :" + req.params.id);
+
     try {
         await UserModel.findOneAndUpdate({ _id: req.params.id }, {
             $set: {
@@ -32,6 +33,18 @@ module.exports.updateUser = async(req, res) => {
             if (!err) return res.send(docs);
             if (err) return res.status(500).send({ message: err })
         })
+    } catch (err) {
+        return res.status(500).json({ message: err });
+    }
+};
+
+module.exports.deleteUser = async(req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID unknown :" + req.params.id);
+
+    try {
+        await UserModel.remove({ _id: req.params.id }).exec();
+        res.status(200).json({ message: "Successfully deleted" });
     } catch (err) {
         return res.status(500).json({ message: err });
     }
