4607d7ae1d8248915ed2703045bd60a83a379931
fbergaut <f.bergaut@gmail.com>
2021-09-29 13:59:25 +0200

    create middleware checkUser avec jwt pour contrôler que le user est bien connecté avec une clé secrète

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 require('dotenv').config({ path: './config/.env' })
 require('./config/db');
+const { checkUser } = require('./middleware/auth.middleware');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cookieParser());
+
+//jwt
+app.get('*', checkUser);
 
 //routes
 app.use('/api/user', userRoutes);
