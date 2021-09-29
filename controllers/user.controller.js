eca441d74cdd8cc35868ff204c327c8a98fdcf7c
fbergaut <f.bergaut@gmail.com>
2021-09-29 11:19:52 +0200

    create PATCH for user follow/unfollow

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -49,3 +49,55 @@ module.exports.deleteUser = async(req, res) => {
         return res.status(500).json({ message: err });
     }
 };
+
+module.exports.follow = async(req, res) => {
+    if (!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToFollow))
+        return res.status(400).send("ID unknown :" + req.params.id);
+
+    try {
+        // add to the follower list
+        await UserModel.findByIdAndUpdate(
+            req.params.id, { $addToSet: { following: req.body.idToFollow } }, { new: true, upsert: true },
+            (err, docs) => {
+                if (!err) res.status(201).json(docs);
+                else return resstatus(400).json(err);
+            }
+        );
+        // add to following list
+        await UserModel.findByIdAndUpdate(
+            req.body.idToFollow, { $addToSet: { followers: req.params.id } },
+            (err, docs) => {
+                // if (!err) res.status(201).json(docs);
+                if (err) return resstatus(400).json(err);
+            }
+        );
+    } catch {
+        return res.status(500).json({ message: err });
+    }
+};
+
+module.exports.unfollow = async(req, res) => {
+    if (!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToUnfollow))
+        return res.status(400).send("ID unknown :" + req.params.id);
+
+    try {
+        // remove from the follower list
+        await UserModel.findByIdAndUpdate(
+            req.params.id, { $pull: { following: req.body.idToUnfollow } }, { new: true, upsert: true },
+            (err, docs) => {
+                if (!err) res.status(201).json(docs);
+                else return resstatus(400).json(err);
+            }
+        );
+        // remove from following list
+        await UserModel.findByIdAndUpdate(
+            req.body.idToUnfollow, { $pull: { followers: req.params.id } },
+            (err, docs) => {
+                // if (!err) res.status(201).json(docs);
+                if (err) return resstatus(400).json(err);
+            }
+        );
+    } catch {
+        return res.status(500).json({ message: err });
+    }
+};
