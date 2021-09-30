7ebfa745e7a02346a9dbdaa7a6eb4fc3120df9d9
fbergaut <f.bergaut@gmail.com>
2021-09-30 13:24:45 +0200

    create fonction updatePost + deletePost

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -6,7 +6,7 @@ module.exports.readPost = (req, res) => {
     PostModel.find((err, docs) => {
         if (!err) res.send(docs);
         else console.log('Error to get data: ' + err);
-    })
+    });
 };
 
 module.exports.createPost = async(req, res) => {
@@ -23,13 +23,35 @@ module.exports.createPost = async(req, res) => {
         return res.status(201).json(post);
     } catch (err) {
         return res.status(400).send(err);
-    }
+    };
 };
 
 module.exports.updatePost = (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID unknown :" + req.params.id);
 
+    const updatedRecord = {
+        message: req.body.message
+    };
+
+    PostModel.findByIdAndUpdate(
+        req.params.id, { $set: updatedRecord }, { new: true },
+        (err, docs) => {
+            if (!err) res.send(docs);
+            else console.log("Update error : " + err);
+        }
+    );
 };
 
 module.exports.deletePost = (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID unknown :" + req.params.id);
 
+    PostModel.findByIdAndRemove(
+        req.params.id,
+        (err, docs) => {
+            if (!err) res.send(docs);
+            else console.log("Delete error : " + err);
+        }
+    );
 };
