ae4bd856b3c85372ee905270e472b2af75471bf9
fbergaut <f.bergaut@gmail.com>
2021-09-30 09:21:06 +0200

    create middleware requireAuth pour connecter automatiquement le user s'il y a un token dans les cookies

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -21,3 +21,19 @@ module.exports.checkUser = (req, res, next) => {
         next();
     }
 };
+
+module.exports.requirAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, process.env.TOKEN_SECRET, async(err, decodedToken) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(decodedToken.id)
+                next();
+            }
+        });
+    } else {
+        console.log('No token')
+    }
+};
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 require('dotenv').config({ path: './config/.env' })
 require('./config/db');
-const { checkUser } = require('./middleware/auth.middleware');
+const { checkUser, requirAuth } = require('./middleware/auth.middleware');
 const app = express();
 
 app.use(bodyParser.json());
@@ -12,6 +12,9 @@ app.use(cookieParser());
 
 //jwt
 app.get('*', checkUser);
+app.get('/jwtid', requirAuth, (req, res) => {
+    res.status(200).send(res.locals.user._id)
+});
 
 //routes
 app.use('/api/user', userRoutes);
